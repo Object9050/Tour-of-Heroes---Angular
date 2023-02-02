1ea1d9417e70a4f05c5f34af2e12dfc991d1cc32
Object9050 <111750980+Object9050@users.noreply.github.com>
2023-02-02 14:41:50 +0100

    Kapitel 4 beendet

---
+++ src/app/hero.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
+
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES);
+    this.messageService.add('HeroService: fetched heroes')
+    return heroes;
+  }
+  // getHeroes(): Hero[] { // OLD
+  //   return HEROES
+  // }
+  
+  constructor(private messageService: MessageService) { }
+
+}
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,22 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+//nicht mehr benötigt. hero.service übernimmt dies // import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'techstarter-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent {
-  heroes = HEROES;
+export class HeroesComponent implements OnInit{
   selectedHero?: Hero;
   
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
+  heroes: Hero[] = [];
+  
+  constructor(private heroService: HeroService, private messageService: MessageService) {}
+  
+  ngOnInit(): void {
+    this.getHeroes();
   }
   
+  onSelect(hero2: Hero): void {
+    this.selectedHero = hero2;
+    this.messageService.add(`HeroesCompoment: Selected hero id=${hero2.id}`);
+  }
   
-  
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);;
+  }
+
   // hero: Hero = { // <-- alt: erste Schritte
   //   id: 1,
   //   name: "Windstorm"
